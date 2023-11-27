2f1fd333ed9b5315591aacc8c0a9c28158f0c3d3
yawtalkstechs <yawtalkstechs@gmail.com>
2023-11-27 21:59:10 +0000

    caching added

---
+++ chapter12/caching.py
@@ -0,0 +1,57 @@
+import os
+import joblib
+import contextlib
+
+from fastapi import FastAPI, Depends, status
+from pydantic import BaseModel
+from sklearn.pipeline import Pipeline
+
+class PredictionOutput(BaseModel):
+    category: str
+
+class PredictionInput(BaseModel):
+    text: str
+
+memory = joblib.memory(location="cache.joblib")
+
+@memory.cache(ignore=["model"])
+def predict(model: Pipeline, text: str) -> int:
+    prediction = model.predict([text])
+    return prediction[0]
+
+class NewsgroupsModel:
+    model: Pipeline | None = None
+    targets: list[str] | None = None
+
+    def load_model(self) -> None:
+        """Loads the model"""
+        model_file = os.path.join(os.path.dirname(__file__), "newsgroups_model.joblib")
+        loaded_model: tuple[Pipeline, list[str]] = joblib.load(model_file)
+        model, targets = loaded_model
+        self.model = model
+        self.targets = targets
+
+    def predict(self, input: PredictionInput) -> PredictionOutput:
+        """Runs a prediction"""
+        if not self.model or not self.targets:
+            raise RuntimeError("Model is not loaded")
+        prediction = predict(self.model, input.text)
+        category = self.targets[prediction]
+        return PredictionOutput(category=category)
+
+newgroups_model = NewsgroupsModel()
+
+@contextlib.asynccontextmanager
+async def lifespan(app: FastAPI):
+    newgroups_model.load_model()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/prediction")
+def prediction(output: PredictionOutput = Depends(newgroups_model.predict),) -> PredictionOutput:
+    return output
+
+@app.delete("/cache", status_code=status.HTTP_204_NO_CONTENT)
+def delete_cache():
+    memory.clear()
