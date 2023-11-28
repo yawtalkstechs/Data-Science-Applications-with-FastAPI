4798cfae8a7fe6d7114fb632f4f7f15b26d8f81b
yawtalkstechs <yawtalkstechs@gmail.com>
2023-11-28 12:33:42 +0000

    Object Detection Added

---
+++ chapter13/object_detection.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+import torch
+from PIL import Image, ImageDraw, ImageFont
+from transformers import YolosForObjectDetection, YolosImageProcessor
+
+root_directory = Path(__file__).parent.parent
+picture_path = root_directory / "assets" / "street.jpg"
+image = Image.open(picture_path)
+
+image_processor = YolosImageProcessor.from_pretrained("hustvl/yolos-tiny")
+model = YolosForObjectDetection.from_pretrained("hustvl/yolos-tiny")
+
+inputs = image_processor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+
+target_sizes = torch.tensor([image.size[::-1]])
+results = image_processor.post_process_object_detection(
+    outputs, target_sizes=target_sizes
+)[0]
+
+draw = ImageDraw.Draw(image)
+font_path = root_directory / "assets" / "OpenSans-ExtraBold.ttf"
+font = ImageFont.truetype(str(font_path), 24)
+for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+    if score > 0.7:
+        box_values = box.tolist()
+        label = model.config.id2label[label.item()]
+        draw.rectangle(box_values, outline="red", width=5)
+        draw.text(box_values[0:2], label, fill="red", font=font)
+image.show()
---
+++ chapter13/object_detection_api.py
@@ -0,0 +1,56 @@
+import contextlib
+
+import torch
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
+from PIL import Image
+from transformers import YolosForObjectDetection, YolosImageProcessor
+
+
+class Object(BaseModel):
+    box: tuple[float, float, float, float]
+    label: str
+
+class Objects(BaseModel):
+    objects: list[Object]
+
+class ObjectDetection:
+    image_processor: YolosImageProcessor | None = None
+    model: YolosForObjectDetection | None = None
+
+    def load_model(self) -> None:
+        self.image_processor = YolosImageProcessor.from_pretrained("hustvl/yolos-tiny")
+        self.model = YolosForObjectDetection.from_pretrained("hustvl/yolos-tiny")
+
+    def predict(self, image: Image.Image) -> Objects:
+        if not self.image_processor or not self.model:
+            raise RuntimeError("Model is not loaded")
+        inputs = self.image_processor(images=image, return_tensors="pt")
+        outputs = self.model(**inputs)
+
+        target_sizes = torch.tensor([image.size[::-1]])
+        results = self.image_processor.post_process_object_detection(
+            outputs, threshold=0.7, target_sizes=target_sizes
+        )[0]
+
+        objects: list[Object] = []
+        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+            if score > 0.7:
+                box_values = box.tolist()
+                label = self.model.config.id2label[label.item()]
+                objects.append(Object(box=box_values, label=label))
+            return Objects(objects=objects)
+
+object_detection = ObjectDetection()
+
+@contextlib.asynccontextmanager
+async def lifespan(app: FastAPI):
+    object_detection.load_model()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/object-detection", response_model=Objects)
+async def post_object_detection(image: UploadFile = File(...)) -> Objects:
+    image_object = Image.open(image.file)
+    return object_detection.predict(image_object)
