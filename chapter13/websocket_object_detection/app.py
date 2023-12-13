fd7f055a27e702a8d905178292054461a87bea17
yawtalkstechs <yawtalkstechs@gmail.com>
2023-12-13 14:53:54 +0000

    chapter 13 & 14 added

---
+++ chapter13/websocket_object_detection/app.py
@@ -0,0 +1,94 @@
+import asyncio
+import contextlib
+import io
+from pathlib import Path
+
+import torch
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from PIL import Image
+from pydantic import BaseModel
+from transformers import YolosForObjectDetection, YolosImageProcessor
+
+class Object(BaseModel):
+    box: tuple[float, float, float, float]
+    label: str
+
+class Objects(BaseModel):
+    objects: list[Object]
+
+class ObjectDetection:
+    image_processor: YolosImageProcessor | None = None
+    model: YolosForObjectDetection | None = None
+
+    def load_model(self) -> None:
+        self.image_processor = YolosImageProcessor.from_pretrained("hustvl/yolos-tiny")
+        self.model = YolosForObjectDetection.from_pretrained("hustvl/yolos-tiny")
+
+    def predict(self, image: Image.Image) -> Objects:
+        if not self.image_processor or not self.model:
+            raise RuntimeError("Model is not loaded")
+        inputs = self.image_processor(images=image, return_tensors="pt")
+        outputs = self.model(**inputs)
+
+        target_sizes = torch.tensor([image.size[::-1]])
+        results = self.image_processor.post_process_object_detection(
+            outputs, target_sizes=target_sizes
+        )[0]
+
+        objects: list[Object] = []
+        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+            if score > 0.7:
+                box_values = box.tolist()
+                label = self.model.config.id2label[label.item()]
+                objects.append(Object(box=box_values, label=label))
+            return Objects(objects=objects)
+
+object_detection = ObjectDetection()
+
+@contextlib.asynccontextmanager
+async def lifespan(app: FastAPI):
+    object_detection.load_model()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+async def receive(websocket: WebSocket, queue: asyncio.Queue):
+    while True:
+        bytes = await websocket.receive_bytes()
+        try:
+            queue.put_nowait(bytes)
+        except asyncio.QueueFull:
+            pass
+
+async def detect(websocket: WebSocket, queue:asyncio.Queue):
+    while True:
+        bytes = await queue.get()
+        image = Image.open(io.BytesIO(bytes))
+        objects = object_detection.predict(image)
+        await websocket.send_json(objects.dict())
+
+@app.websocket("/object-detection")
+async def ws_object_detection(websocket: WebSocket):
+    await websocket.accept()
+    queue: asyncio.Queue = asyncio.Queue(maxsize=1)
+    receive_task = asyncio.create_task(receive(websocket, queue))
+    detect_task = asyncio.create_task(detect(websocket, queue))
+    try:
+        done, pending = await asyncio.wait(
+            {receive_task, detect_task}
+        )
+        for task in pending:
+            task.cancel()
+        for task in done:
+            task.result()
+    except WebSocketDisconnect:
+        pass
+
+@app.get("/")
+async def index():
+    return FileResponse(Path(__file__).parent / "index.html")
+
+static_files_app = StaticFiles(directory=Path(__file__).parent / "assets")
+app.mount("/assets", static_files_app)
---
+++ chapter14/text_to_image.py
@@ -0,0 +1,65 @@
+from collections.abc import Callable
+
+import torch
+from diffusers import StableDiffusionPipeline
+from PIL import Image
+
+
+class TextToImage:
+    pipe: StableDiffusionPipeline | None = None
+
+    def load_model(self) -> None:
+    	# Enable CUDA GPU
+    	if torch.cuda.is_available():
+        	device = "cuda"
+    	# Enable Apple Silicon (M1) GPU
+    	elif torch.backends.mps.is_available():
+        	device = "mps"
+    	# Fallback to CPU
+    	else:
+        	device = "cpu"
+
+    	pipe = StableDiffusionPipeline.from_pretrained(
+        	"runwayml/stable-diffusion-v1-5",
+        	callback_steps=50,  # Set an integer value here
+    	)
+    	pipe.to(device)
+    	self.pipe = pipe
+
+    def generate(
+        self,
+        prompt: str,
+        *,
+        negative_prompt: str | None = None,
+        num_steps: int = 50,
+        callback: Callable[[int, int, torch.FloatTensor], None] | None = None,
+    ) -> Image.Image:
+        if not self.pipe:
+            raise RuntimeError("Pipeline is not loaded")
+
+        # Set callback_steps to a default value if it's None
+        callback_steps = 1 if callback is not None else None
+
+        return self.pipe(
+            prompt,
+            negative_prompt=negative_prompt,
+            num_inference_steps=num_steps,
+            guidance_scale=9.0,
+            callback=callback,
+            callback_steps=callback_steps,
+        ).images[0]
+
+
+if __name__ == "__main__":
+    text_to_image = TextToImage()
+    text_to_image.load_model()
+
+    def callback(step: int, _timestep, _tensor):
+        print(f"🚀 Step {step}")
+
+    image = text_to_image.generate(
+        "A Renaissance castle in the Loire Valley",
+        negative_prompt="low quality, ugly",
+        callback=callback,
+    )
+    image.save("output.png")
